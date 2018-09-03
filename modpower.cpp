1ca57bce78a910e91b663101bb25324102d093ab
Alexa Huang <hyzale@berkeley.edu>
2018-09-03 13:34:05 -0400

    apply incremental modulus

--- modpower.cpp
+++ modpower.cpp
@@ -2,14 +2,14 @@
  #include <iostream>
  using namespace std;
  typedef unsigned long long ull;
- 
+
  ull modpower(ull base, ull exponent, ull modulus) {
     if (exponent == 0) {
         return 1 % modulus;
     }
     ull result = modpower(base, exponent / 2, modulus);
-    result = (result * result) % modulus;
-    return (exponent % 2 == 0) ? result : (base * result) % modulus;
+    result = (result % modulus * result % modulus) % modulus;
+    return (exponent % 2 == 0) ? result : (base % modulus * result % modulus ) % modulus;
  }
 
  int main() {
