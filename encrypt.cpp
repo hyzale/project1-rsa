c5bcd9539c6e6507c6c968d1cf5200677b17f32f
Alexa Huang <hyzale@berkeley.edu>
2018-09-03 16:51:54 -0400

    encrypting file

--- encrypt.cpp
+++ encrypt.cpp
@@ -1,17 +1,23 @@
 
- #include <iostream>
- using namespace std;
- typedef unsigned long long ull;
-
+#include <iostream>
+#include <fstream>
+using namespace std;
+typedef unsigned long long ull; 
+ 
  ull modpower(ull base, ull exponent, ull modulus) {
-    if (exponent == 0) {
-        return 1 % modulus;
-    }
+    if (exponent == 0) return 1 % modulus;
     ull result = modpower(base, exponent / 2, modulus);
-    result = (result % modulus * result % modulus) % modulus;
-    return (exponent % 2 == 0) ? result : (base % modulus * result % modulus ) % modulus;
+    result = (result * result) % modulus;
+    return (exponent % 2 == 0) ? result : (base * result) % modulus;
  }
 
  int main() {
-
+    ull e; ull n; char x;
+    ifstream encryptFile("public.txt");
+    ifstream fin("plaintext.txt");
+    ofstream fout("encrpyted.txt");
+    encryptFile >> e >> n;
+    while(fin.get(x)){
+        fout << modpower(x, e, n) << endl;
+    } 
  }
