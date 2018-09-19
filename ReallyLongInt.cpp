a5b438b5688e99b5167f24ddb1a28fe2f988932c
Alexa Huang <hyzale@berkeley.edu>
2018-09-18 22:48:08 -0400

    finish work before 15

---
+++ ReallyLongInt.cpp
@@ -0,0 +1,118 @@
+#include <string>
+#include <iostream>
+#include <cmath>
+
+#include "ReallyLongInt.hpp"
+
+using namespace std;
+
+//Defult Constructor to represent 0
+ReallyLongInt::ReallyLongInt() {
+    isNeg = false;
+    digits = 0;
+    numDigits = 1;
+}
+
+
+//Private method to remove leading zeroes
+void ReallyLongInt::removeLeadingZeros(unsigned* x, unsigned& xSize) const {
+    int count = -1;
+    for (int i = 0; i < xSize; i++) {
+        if (x[i] != 0) {
+            count = i;
+            break;
+        }
+    }
+   
+    if (count == -1) {
+        xSize = 1;
+        x[0] = 0;
+    } else {
+        for (int i = 0; i < xSize - count; i++) {
+            x[i] = x[count + i];
+        }
+        xSize -= count;
+    }   
+
+/*change to iterative method after comment from checkpoint 2
+change to iterative method after comment from checkpoint 2
+    if (x[0] == 0 && xSize != 1) {
+        for (int i = 0; i < xSize - 1; i++) {
+            x[i] = x[i + 1];
+        }
+        xSize --;
+        removeLeadingZeros(x, xSize);
+
+    }    */
+
+}
+
+//String Construstor
+ReallyLongInt::ReallyLongInt(const string& numStr) {
+    isNeg = (numStr[0] == '-');
+    numDigits = numStr.size();
+    unsigned* numArr = new unsigned[numDigits];
+    
+    if (isNeg) {
+        for (int i = 0; i < numDigits - 1; i++) {
+            numArr[i] = numStr[i + 1] - 48;
+        }
+        numDigits --;    
+    } else {
+        for (int i = 0; i < numDigits; i++) {
+            numArr[i] = numStr[i] - 48;
+        }    
+    }
+    removeLeadingZeros(numArr, numDigits);
+    digits = numArr;
+    if (digits[0] == 0) {isNeg = false;}
+}
+
+//Destructor
+ReallyLongInt::~ReallyLongInt() {
+    delete digits;
+}
+
+//toString method
+string ReallyLongInt::toString() const {
+    string numStr;
+    if (isNeg) {numStr = "-";}
+    for (int i = 0; i < numDigits; i++) {
+        numStr += to_string(digits[i]);
+    }
+    return numStr;
+}
+
+//Constructor from numbers
+ReallyLongInt::ReallyLongInt(long long num) {
+
+}
+
+//Copy Constructor
+
+ReallyLongInt::ReallyLongInt(const ReallyLongInt& other) {
+    
+}
+
+
+/*--------------------------
+----------Test Area---------
+---------------------------*/
+
+
+unsigned ReallyLongInt::getNumDigits() {
+    return numDigits;
+}
+
+bool ReallyLongInt::getIsNeg() {
+    return isNeg;
+}
+
+int main(int argc, char const *argv[])
+{
+    ReallyLongInt* a = new ReallyLongInt("002001");
+    cout << a->toString() << endl;
+    cout << a->getNumDigits() << endl;
+    cout << a->getIsNeg() << endl;
+
+}
---
+++ ReallyLongInt.hpp
@@ -0,0 +1,35 @@
+#ifndef REALLYLONGINT_H
+#define REALLYLONGINT_H
+#include <string>
+#include <cstring>
+#include <string.h>
+
+
+using namespace std;
+
+
+class ReallyLongInt 
+{
+private:
+    bool isNeg;
+    const unsigned* digits;
+    unsigned numDigits;
+
+    void removeLeadingZeros(unsigned* x, unsigned& xSize) const;
+    ReallyLongInt(unsigned* digitsArr, unsigned arrSize, bool isNeg);
+public:
+    ReallyLongInt();
+    ReallyLongInt(const string& numStr);
+    ~ReallyLongInt();
+    string toString() const;
+    ReallyLongInt(long long num);
+    ReallyLongInt(const ReallyLongInt& other);
+
+
+/*--------Test Method--------*/
+    unsigned getNumDigits();
+    bool getIsNeg();
+};
+
+
+#endif
