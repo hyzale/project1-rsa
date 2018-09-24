73ef1fc1c4a8fa534196c0c3192fc4370c492317
Alexa Huang <hyzale@berkeley.edu>
2018-09-24 05:07:49 -0400

    add test for ReallyLongInt

---
+++ ReallyLongInt_TEST.cpp
@@ -0,0 +1,102 @@
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+#include <iostream>
+
+#include "ReallyLongInt.hpp"
+
+unsigned ReallyLongInt::getNumDigits() {
+    return numDigits;
+}
+
+bool ReallyLongInt::getIsNeg() {
+    return isNeg;
+}
+
+TEST_CASE ("Test for ReallyLongInt class") {
+
+    SECTION ("Testing remove leading zero") {
+        ReallyLongInt* a = new ReallyLongInt("-000002001");
+        ReallyLongInt* b = new ReallyLongInt("-00000");
+        REQUIRE (a->getIsNeg() == true );
+        REQUIRE (b->getIsNeg() == false );
+        REQUIRE (a->getNumDigits() == 4 );
+        REQUIRE (b->getNumDigits() == 1 );
+        delete a;
+        delete b;
+    }
+    
+    SECTION ("Testing tostring") {
+        ReallyLongInt* a = new ReallyLongInt("-000002001");
+        ReallyLongInt* b = new ReallyLongInt("-00000");
+        REQUIRE (a->toString() == "-2001" );
+        REQUIRE (b->toString() == "0" );
+        delete a;
+        delete b;
+    }
+
+    SECTION ("Testing constructor from number") {
+        ReallyLongInt* a = new ReallyLongInt(-2001);
+        ReallyLongInt* b = new ReallyLongInt(-0);
+        ReallyLongInt* c = new ReallyLongInt(2001);
+        REQUIRE (a->getIsNeg() == true );
+        REQUIRE (b->getIsNeg() == false );
+        REQUIRE (c->getIsNeg() == false );
+        REQUIRE (a->getNumDigits() == 4 );
+        REQUIRE (b->getNumDigits() == 1 );
+        REQUIRE (c->getNumDigits() == 4 );
+        REQUIRE (a->toString() == "-2001" );
+        REQUIRE (b->toString() == "0" );
+        REQUIRE (c->toString() == "2001" );
+        delete a;
+        delete b;
+        delete c;
+    }
+    SECTION ("Testing copy constructor") {
+        ReallyLongInt* a = new ReallyLongInt();
+        ReallyLongInt* b = new ReallyLongInt(*a);
+        delete a;
+        a = new ReallyLongInt(2003);
+        ReallyLongInt* c = new ReallyLongInt(-2001);
+        ReallyLongInt* d = new ReallyLongInt(*c);
+        REQUIRE (b->getIsNeg() == false );
+        REQUIRE (c->getIsNeg() == true );
+        REQUIRE (b->getNumDigits() == 1 );
+        REQUIRE (c->getNumDigits() == 4 );
+        REQUIRE (b->toString() == "0" );
+        REQUIRE (c->toString() == "-2001" );
+        delete a;
+        delete b;
+        delete c;
+        delete d;
+
+    }
+
+    SECTION ("Testing equal method") {
+        ReallyLongInt* a = new ReallyLongInt(1984);
+        ReallyLongInt* b = new ReallyLongInt("1984");
+        ReallyLongInt* c = new ReallyLongInt(-2001);
+        ReallyLongInt* d = new ReallyLongInt(-2002);
+        REQUIRE (a->equal(*b) == true );
+        REQUIRE (c->equal(*d) == false );
+        delete a;
+        delete b;
+        delete c;
+        delete d;
+
+    }
+
+    SECTION ("Testing greater method") {
+        ReallyLongInt* a = new ReallyLongInt(1984);
+        ReallyLongInt* b = new ReallyLongInt("1984");
+        ReallyLongInt* c = new ReallyLongInt(-2001);
+        ReallyLongInt* d = new ReallyLongInt(-2002);
+        REQUIRE (a->greater(*b) == false );
+        REQUIRE (c->greater(*d) == true );
+        delete a;
+        delete b;
+        delete c;
+        delete d;
+
+    }  
+
+}
